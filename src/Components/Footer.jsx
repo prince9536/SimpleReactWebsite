d7064886e8e47c6ffdb649649407844903bf6ad8
prince9536 <princebhardwajp48@gmail.com>
2024-04-19 23:06:41 +0530

    changes in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
   <div>
     <h5>Follow Us</h5>
     <div>
-      <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+      <a href={"/"} target={"blank"}>
         Youtube
       </a>
       <a href="" target={"blank"}>
